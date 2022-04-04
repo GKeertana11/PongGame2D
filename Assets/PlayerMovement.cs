ff8ec4253b70feb20b46b1a72298c7032b409fa8
KeertanaG <keertana.govte@yesgnome.com>
2022-04-04 15:57:46 +0530

    Ui created

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    Rigidbody2D rb;
+    public Vector2 movement;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        movement.y= Input.GetAxis("Vertical1");
+        rb.velocity = new Vector2(0, movement.y*speed);
+      //  movement.y = Input.GetAxis("Vertical");
+
+      /*  if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("1");
+           
             transform.Translate(0,speed,0);
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             transform.Translate(0,-speed,0);
-        }
+        }*/
     }
+  
 }
